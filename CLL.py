883213eec7f25970c7e9da52bfcc0a32e456341c
Mohit Raut <workitoutmohit9@gmail.com>
2024-09-21 16:02:31 +0530

    CLL Assignment

--- CLL.py
+++ CLL.py
@@ -11,23 +11,60 @@ class CLL:
         return self.last==None
     
     def insert_at_start(self,data):
-        n=(data,self.last.next)
-        while self.last != None:
+        n= Node(data)
+        if self.is_empty:
+            n.next=n
+            self.last=n
+        else:
             n.next=self.last.next
-        self.last=n
+            self.last.next=n  
+        
+    def insert_at_last(self,data):
+        n= Node(data,None)
+        if self.is_empty():
+            n.next=n
+            self.last=n
+        else:
+            n.next=self.last.next
+            self.last.next=n
+            self.last=n
+            
+    def search(self,data):
+        if self.is_empty():
+            return None
+        temp=self.last.next
+        while temp != self.last:
+            if temp.item==data:
+                return temp
+            temp=temp.next
+        if temp.item==data:          #for last node
+            return temp
+        return None
+            
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n=Node(data,temp.next)
+            temp.next=n
+            if temp==self.last:
+                self.last=n
+        
+        
+        
         
     def print_list(self):
-        temp=self.last.next
-        while temp != self.last.next:
-            print(temp,end=' ')
-            temp=temp.next
+        if not self.is_empty():
+            temp=self.last.next
+            while temp != self.last:
+                print(temp.item,end=' ')
+                temp=temp.next
+            print(temp.item)
+            
         
 #driver code
 mylist =CLL()
 mylist.insert_at_start(10)
+mylist.insert_after(mylist.search(10),20)
 mylist.print_list()
-print()
-print()
 print()
 
     
