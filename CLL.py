8467054b094ee66664f03a687f2335499f783e1b
Mohit Raut <workitoutmohit9@gmail.com>
2024-09-22 01:10:17 +0530

    CLL assignment

--- CLL.py
+++ CLL.py
@@ -47,10 +47,7 @@ class CLL:
             temp.next=n
             if temp==self.last:
                 self.last=n
-        
-        
-        
-        
+                
     def print_list(self):
         if not self.is_empty():
             temp=self.last.next
@@ -59,12 +56,87 @@ class CLL:
                 temp=temp.next
             print(temp.item)
             
+    def delete_first (self):
+        if not self.is_empty():
+            if self.last.next==self.last:
+                self.last=None 
+            else:
+                self.last.next=self.last.next.next
+    
+    def delete_last(self):
+        if not self.is_empty():
+            if self.last.next==self.last:
+                self.last=None
+            else:
+                temp=self.last.next
+                while temp.next!=self.last:
+                    temp=temp.next
+                temp.next=self.last.next
+                self.last=temp
+                
+    def delete_item(self,data):
+        if not self.is_empty():
+            if self.last.next==self.last:
+                if self.last.item==data:
+                    self.last=None
+            else:
+                if self.last.next.item==data:
+                    self.delete_first()
+                else:    
+                    temp=self.last.next
+                    while temp!=self.last:
+                        if temp.next==self.last:
+                            if self.last.item==data:
+                                self.delete_last()
+                                break
+                        if temp.next.item==data:
+                            temp.next=temp.next.next
+                            break
+                        temp=temp.next
+    def __iter__(self):
+        if self.last==None:
+            return CLLIterator(None)
+        else:
+            return CLLIterator(self.last.next)
+                                        
+class CLLIterator:
+    def __init__(self,start):
+        self.current=start
+        self.start=start
+        self.count=0  
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current==None:
+            raise StopIteration  
+        if self.current==self.start and self.count==1:
+            raise StopIteration
+        else:
+            self.count=1
+        data= self.current.item
+        self.current=self.current.next
+        return data
+       
+                                      
+        
+        
+                
+        
+        
         
 #driver code
 mylist =CLL()
 mylist.insert_at_start(10)
-mylist.insert_after(mylist.search(10),20)
-mylist.print_list()
+mylist.insert_at_last(30)
+mylist.insert_at_last(40)
+mylist.insert_after(mylist.search(10),15)
+for x in mylist:
+    print (x,end=' ')
+print()
+
+mylist.delete_item(10)
+for x in mylist:
+    print(x,end=' ')
 print()
 
     
