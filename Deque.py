282be1ac1364a0b4bff9b06f4d44910e1555509b
Mohit Raut <workitoutmohit9@gmail.com>
2024-10-05 23:23:02 +0530

    C:/Users/Dell/AppData/Local/Microsoft/WindowsApps/python3.11.exe c:/Users/Dell/OneDrive/Desktop/Mohit/git/DSA-py/Deque.py

---
+++ Deque.py
@@ -0,0 +1,49 @@
+class Deque:
+    def __init__(self):
+        self.items=[]
+    
+    def is_empty(self):
+        return len(self.items)==0
+    
+    def insert_front(self,data):
+        self.items.insert(0,data) 
+        
+    def insert_rear(self,data):
+        self.items.append(data)
+        
+    def delete_front(self):
+        if self.is_empty():
+            raise IndexError("Deque is Empty")
+        else:
+            self.items.pop(0)
+            
+    def delete_rear(self):
+        if self.is_empty():
+            raise IndexError("Deque is Empty")
+        else:
+            self.items.pop()
+        
+    def get_front(self):
+        if self.is_empty():
+            raise IndexError("Deque is Empty")
+        else:
+            return self.items[0]
+        
+    def get_rear(self):
+        if self.is_empty():
+            raise IndexError("Deque is Empty")
+        else:
+            return self.items[-1]
+        
+    def size(self):
+        return len(self.items)
+        
+ #driver code
+d1=Deque()
+d1.insert_front(10) 
+d1.insert_rear(20) 
+d1.insert_front(30) 
+d1.insert_rear(40)  
+print(d1.get_front(),d1.get_rear())
+       
+        
