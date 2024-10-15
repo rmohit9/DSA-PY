2588a9dc6698455367aa0aec7554162ab77825e5
Mohit Raut <workitoutmohit9@gmail.com>
2024-10-15 23:06:03 +0530

    Class and Object concept

--- Employee.py
+++ Employee.py
@@ -1,44 +1,49 @@
 #instance Method
 
-# class Test:
-#     x=2
-#     def __init__(self,a,b):
-#         self.a=a
-#         self.b=b        
-#     def show(self):
-#         print(self.a,self.b)
-#     @staticmethod
-#     def f2():
-#         print("Hello")
-#     @classmethod
-#     def f3(cls):
-#         print(cls.x)
+class Test:
+    x=2
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b        
+    def show(self):
+        print(self.a,self.b)
+    @staticmethod
+    def f2():
+        print("Hello")
+    @classmethod
+    def f3(cls):
+        print(cls.x)
     
 
-# t1= Test(3,4)
-# t2= Test(5,6)
-# t1.show()
-# t2.show()
-# Test.f3()    #calling class method
-# Test.f2()    #calling static method
+t1= Test(3,4)
+t2= Test(5,6)
+t1.show()
+t2.show()
+Test.f3()    #calling class method
+Test.f2()    #calling static method
+
+# o/p:  3 4
+#       5 6
+#       2
+#       Hello
             
                            
   
     # #Instance Method- it should always have an argument 'self' with NO DECORATOR 
     # variable inside instance object is called instance variable
-    # def f1(self):
-    #     pass
+        # def f1(self):
+        #     pass
     
     # #Static Method-It is your choice to have or dont have arguments but it should contain a decorator (@staticmethod) 
     # (variable inside the class object are called static variable)
-    # def f2():
-    #     pass
-    # @staticmethod
+        # def f2():
+        #     pass
+        # @staticmethod
     
     # #Class Method-It should contain atleast one argument along with the decorator (@classmethod)
-    # def f3(cls):
-    #     pass
-    # @classmethod
+        # def f3(cls):
+        #     pass
+        # @classmethod
     
     
 class Employee:
@@ -66,3 +71,6 @@ e1.setName("Harshal")
 e1.setSalary(45000)
 print(e1.getEmpid(),e1.getName(),e1.getSalary())
 print(e2.getEmpid(),e2.getName(),e2.getSalary())
+
+# o/p:  45000 Harshal 45000
+#       11100 Mohit 11100
